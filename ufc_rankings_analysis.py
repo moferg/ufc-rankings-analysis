bc3ba51165cf0a1fb459ba4a7ee91266663a85fb
Marshall Ferguson <moferg01@louisville.edu>
2020-11-10 16:51:05 -0500

    added outline

---
+++ ufc_rankings_analysis.py
@@ -0,0 +1,10 @@
+# Marshall Ferguson - 11/2020
+
+# OUTLINE
+# Create a DB from CSV file
+# Use CSV file for data viz
+# Use DB for data analysis
+# Have options for user to choose between graphs or text
+# Potential options: duration in rankings (longest/shortest), info of a fighter given fighter's name,
+                   # info for a weight class, info for P4P, duration as champ (longest/shortest),
+                   # fighters who were champs multiple times, fighters who were champs in multiple weight classes
