4327cc1a35399a42644c6553b38e81626c8956a4
Marshall Ferguson <moferg01@louisville.edu>
2020-11-12 06:32:01 -0500

    added unique_values() and changed a few variable names

--- ufc_rankings_analysis.py
+++ ufc_rankings_analysis.py
@@ -14,14 +14,22 @@
 import csv
 import numpy as np
 
+# Functions
+
+def unique_values(list1):
+    '''Takes a list as input and outputs a list with all duplicates removed'''
+    x = np.array(list1)
+    return list(np.unique(x))
+
 # Variables created before opening CSV file
 
-date =[]
+dates =[]
 weight_class = []
-fighter_name = []
+names = []
 rank = []
+filepath = 'rankings_history.csv'
 
-with open('rankings_history.csv') as csv_file:
+with open(filepath) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -29,16 +37,29 @@ with open('rankings_history.csv') as csv_file:
             print(f'Column names: {", ".join(row)}')
             line_count += 1
         else:
-            date.append(row[0])
+            dates.append(row[0])
             weight_class.append(row[1])
-            fighter_name.append(row[2])
+            names.append(row[2])
             rank.append(row[3])
             line_count +=1
 
-with open('rankings_history.csv') as csv_file:
+with open(filepath) as csv_file:
     csv_dict_reader = csv.DictReader(csv_file)
     for dicts in csv_dict_reader:
         if dicts.get('rank') == '0':
-            print(f'{dicts.get("fighter")} was the {dicts.get("weightclass")} champ on {dicts.get("date")}')
+            # print(f'{dicts.get("fighter")} was the {dicts.get("weightclass")} champ on {dicts.get("date")}')
+            pass
 
 print(f'Successfully processed {line_count} lines.')
+
+# Remove duplicates from names list
+unique_names = unique_values(names)
+print(f'The value of the unique_names variable is : {unique_names}')
+print(f'The length of the unique_names variable is : {len(unique_names)}')
+
+weeks_ranked = [names.count(name) for name in unique_names]
+print(f'The value of the weeks_ranked variable is : {weeks_ranked}')
+print(f'The length of the weeks_ranked variable is : {len(weeks_ranked)}')
+
+for a, b in zip(unique_names, weeks_ranked):
+    print(f'Fighter {a} spent {b} weeks in the rankings')
