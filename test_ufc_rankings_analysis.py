c7503a9ed6771e38221e18ca1e887e6fc472ab43
Marshall Ferguson <moferg01@louisville.edu>
2020-11-13 15:52:22 -0500

    added test_variable_types()

--- test_ufc_rankings_analysis.py
+++ test_ufc_rankings_analysis.py
@@ -4,9 +4,12 @@
 
 import unittest
 import ufc_rankings_analysis
+from pandas import DataFrame
 
-# class TestUfcRankingsAnalysis(unittest.TestCase):
+class TestUfcRankingsAnalysis(unittest.TestCase):
 
+    def test_variable_types(self):
+        self.assertEqual(type(ufc_rankings_analysis.df), DataFrame)
 
 if __name__ == "__main__":
     unittest.main()
