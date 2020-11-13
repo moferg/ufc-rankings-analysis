16b72befc9758142ef0b60805d0c950673b33ff3
Marshall Ferguson <moferg01@louisville.edu>
2020-11-13 12:18:34 -0500

    removed unneeded tests after changing data to dataframes

--- test_ufc_rankings_analysis.py
+++ test_ufc_rankings_analysis.py
@@ -5,16 +5,8 @@
 import unittest
 import ufc_rankings_analysis
 
-class TestUfcRankingsAnalysis(unittest.TestCase):
+# class TestUfcRankingsAnalysis(unittest.TestCase):
 
-    def test_len_of_csv(self):
-        self.assertEqual(ufc_rankings_analysis.line_count, 41431)
-
-    def test_len_of_lists(self):
-        self.assertEqual(len(ufc_rankings_analysis.date), ufc_rankings_analysis.line_count - 1)
-        self.assertEqual(len(ufc_rankings_analysis.weight_class), ufc_rankings_analysis.line_count - 1)
-        self.assertEqual(len(ufc_rankings_analysis.fighter_name), ufc_rankings_analysis.line_count - 1)
-        self.assertEqual(len(ufc_rankings_analysis.rank), ufc_rankings_analysis.line_count - 1)
 
 if __name__ == "__main__":
     unittest.main()
